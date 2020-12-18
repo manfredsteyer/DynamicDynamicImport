6d8c2cde094bbfc2290bcbd15f8204d491188034
Manfred Steyer <manfred.steyer@gmx.net>
2020-12-19 00:18:51 +0100

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demo3';
+export class AppComponent  {
+  title = 'Hello World!';
+
+  constructor() {
+    let fileName = './test';
+    import(fileName).then(m => console.debug('sum', m.fn(1,2)));
+  }
 }
